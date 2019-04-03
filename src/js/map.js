45e44d2306b5440007fe000612bb36329ddbc32e
stufi04 <st.ivanov44@gmail.com>
2019-04-03 20:57:25 +0200

    map shows region names and is color coded

--- src/js/map.js
+++ src/js/map.js
@@ -1,26 +1,14 @@
-// Prepare random data
-var data = [
-    ['DE.SH', 728],
-    ['DE.BE', 710],
-    ['DE.MV', 963],
-    ['DE.HB', 541],
-    ['DE.HH', 622],
-    ['DE.RP', 866],
-    ['DE.SL', 398],
-    ['DE.BY', 785],
-    ['DE.SN', 223],
-    ['DE.ST', 605],
-    ['DE.NW', 237],
-    ['DE.BW', 157],
-    ['DE.HE', 134],
-    ['DE.NI', 136],
-    ['DE.TH', 704],
-    ['DE.', 361]
-];
-
 $.getJSON('https://raw.githubusercontent.com/martgnz/bcn-geodata/master/barris/barris_geo.json', function (geojson) {
 
-    console.log(geojson)
+    var data = []
+    $.each(geojson.features, function (index, feature) {
+        data.push({
+            "N_Barri": feature.properties['N_Barri'],
+            value: feature.properties['C_Barri']
+        });
+    });
+
+    console.log(data)
 
     // Initiate the chart
     Highcharts.mapChart('map', {
@@ -40,26 +28,33 @@ $.getJSON('https://raw.githubusercontent.com/martgnz/bcn-geodata/master/barris/b
         },
 
         colorAxis: {
-            tickPixelInterval: 100
+            tickPixelInterval: 100,
+            min: 0,
+            max: 80
         },
 
         series: [{
             data: data,
-            keys: ['code_hasc', 'value'],
-            joinBy: 'code_hasc',
+            mapData: geojson,
+            joinBy: 'N_Barri',
             name: 'Random data',
             states: {
                 hover: {
-                    color: '#a4edba'
+                    color: '#a4edba',
+                    tooltip: {
+                        enabled: false
+                    }
                 }
             },
             dataLabels: {
                 enabled: true,
-                format: '{point.properties.N_Barri}'
+                format: '{point.N_Barri}'
             }
         }]
+
     });
 
 
 });
+
 
